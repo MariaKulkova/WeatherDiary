39bae3426cd389daac457ee16545954ed7fc6116
MariaKulkova <mkulkova@distillery.com>
2017-05-07 22:59:26 +0300

    Add jquery with typings

---
+++ scripts/login.js
@@ -0,0 +1,4 @@
+/// <reference path="typings/jquery.d.ts"/>
+$(function () {
+    $("#login-button").click(function (e) { console.log(e); alert("Привет мир"); });
+});
---
+++ scripts/login.ts
@@ -0,0 +1,6 @@
+/// <reference path="typings/jquery.d.ts"/>
+
+$(() => {
+    $("#login-button").click((e) => { console.log(e);alert("Привет мир"); });
+});
+
