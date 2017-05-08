494ca027838dc241202baa6964a29f3035611c3c
MariaKulkova <mkulkova@distillery.com>
2017-05-08 22:55:22 +0300

    Implement Auth module

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,23 @@
 /// <reference path="typings/jquery.d.ts"/>
+/// <reference path="./KinveyAuth.ts"/>
+var LoginForm = (function () {
+    function LoginForm(container, authManager) {
+        this.container = container;
+        this.authManager = authManager;
+        console.log("In constructor");
+        var form = $(this.container);
+        form.submit(function (e) {
+            e.preventDefault();
+            console.log("Signup! Yeah!");
+        });
+    }
+    return LoginForm;
+}());
 $(function () {
-    $("#login-button").click(function (e) { console.log(e); alert("Привет мир"); });
+    Kinvey.initializeKinvey();
+    var loginActionForm = document.getElementById("login-action-form");
+    console.log(loginActionForm);
+    var manager = new Kinvey.AuthManager();
+    var login = new LoginForm(loginActionForm, manager);
 });
+//# sourceMappingURL=login.js.map
