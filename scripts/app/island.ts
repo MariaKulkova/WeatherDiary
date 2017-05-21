bb62621a244e5fd67777492441a69a8a843485e3
MariaKulkova <mkulkova@distillery.com>
2017-05-22 01:26:03 +0300

    Implement wind force slider

--- scripts/app/island.ts
+++ scripts/app/island.ts
@@ -1,12 +1,19 @@
 declare var d3: any;
 /// <reference path="./circle-slider.ts"/>
 
-let svg = d3.select("svg.slider-container")
+let svg = d3.select("svg.sun-slider-container")
 let sunAngleCallback: (value: number) => void = function(angle) { 
     let formattedValue = Math.round(120 * angle - 60)
     d3.select("p.temperature-value").text(formattedValue)
 };
-
 let rotate = new Slider.RotateAttributes(1, 1, 0.95, 180, 90)
 let sunSlider = new Slider.CircleSlider(svg, "img/sun-plain.png", 0.4, rotate, sunAngleCallback)
 sunSlider.render()
+
+let svgWind = d3.select("svg.wind-slider-container")
+let windAngleCallback: (value: number) => void = function(angle) { 
+    let formattedValue = Math.round(120 * angle - 60)
+};
+let rotateWind = new Slider.RotateAttributes(0.5, 0.5, 0.5, 225, -45)
+let windSlider = new Slider.CircleSlider(svgWind, "img/windforce-drag-element.png", 0.3, rotateWind, windAngleCallback)
+windSlider.render()
--- scripts/js/island.js
+++ scripts/js/island.js
@@ -1,5 +1,5 @@
 /// <reference path="./circle-slider.ts"/>
-let svg = d3.select("svg.slider-container");
+let svg = d3.select("svg.sun-slider-container");
 let sunAngleCallback = function (angle) {
     let formattedValue = Math.round(120 * angle - 60);
     d3.select("p.temperature-value").text(formattedValue);
@@ -7,4 +7,11 @@ let sunAngleCallback = function (angle) {
 let rotate = new Slider.RotateAttributes(1, 1, 0.95, 180, 90);
 let sunSlider = new Slider.CircleSlider(svg, "img/sun-plain.png", 0.4, rotate, sunAngleCallback);
 sunSlider.render();
+let svgWind = d3.select("svg.wind-slider-container");
+let windAngleCallback = function (angle) {
+    let formattedValue = Math.round(120 * angle - 60);
+};
+let rotateWind = new Slider.RotateAttributes(0.5, 0.5, 0.5, 225, -45);
+let windSlider = new Slider.CircleSlider(svgWind, "img/windforce-drag-element.png", 0.3, rotateWind, windAngleCallback);
+windSlider.render();
 //# sourceMappingURL=island.js.map
